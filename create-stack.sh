2055666313584394d107ff29501119e318f6775d
Thomas Weibel <thom@ikhono.net>
2020-05-10 19:45:20 +0200

    Update create and update stack scripts

--- create-stack.sh
+++ create-stack.sh
@@ -1,19 +1,23 @@
-# 1. Create EKS Cluster Role
-aws cloudformation create-stack\
-  --stack-name eks-cluster-role\
-  --template-body file://cfn/eks-cluster-role.yml\
-  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
-  --region=us-west-2
-# 2. Create EKS Cluster VPC
+# 1. Create EKS Cluster VPC
 aws cloudformation create-stack\
   --stack-name eks-vpc\
   --template-body file://cfn/eks-vpc.yml\
+  --parameters file://cfn/parameters.json \
   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
   --region=us-west-2
 
-# 3. Create EKS WorkerNode Group Role
+# 2. Create EKS WorkerNode Group Role
 aws cloudformation create-stack\
   --stack-name eks-node-group-role\
   --template-body file://cfn/eks-node-group-role.yml\
+  --parameters file://cfn/parameters.json \
+  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
+  --region=us-west-2
+
+# 3. Create EKS Cluster
+aws cloudformation create-stack\
+  --stack-name eks-cluster\
+  --template-body file://cfn/eks-cluster.yml\
+  --parameters file://cfn/parameters.json \
   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
   --region=us-west-2

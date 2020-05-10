844a6823956c4b7bcbc4cd452bec6b557c589fcf
Thomas Weibel <thom@ikhono.net>
2020-05-10 17:09:17 +0200

    Add EKS node group role

--- create-stack.sh
+++ create-stack.sh
@@ -4,10 +4,16 @@ aws cloudformation create-stack\
   --template-body file://cfn/eks-cluster-role.yml\
   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
   --region=us-west-2
-
 # 2. Create EKS Cluster VPC
 aws cloudformation create-stack\
   --stack-name eks-vpc\
   --template-body file://cfn/eks-vpc.yml\
+  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
+  --region=us-west-2
+
+# 3. Create EKS WorkerNode Group Role
+aws cloudformation create-stack\
+  --stack-name eks-node-group-role\
+  --template-body file://cfn/eks-node-group-role.yml\
   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
   --region=us-west-2

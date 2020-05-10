c03dad4a8de3ffb1083069b5cccb116d7104a4c4
Thomas Weibel <thom@ikhono.net>
2020-05-10 16:45:04 +0200

    Add EKS VPC

--- create-stack.sh
+++ create-stack.sh
@@ -4,3 +4,10 @@ aws cloudformation create-stack\
   --template-body file://cfn/eks-cluster-role.yml\
   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
   --region=us-west-2
+
+# 2. Create EKS Cluster VPC
+aws cloudformation create-stack\
+  --stack-name eks-vpc\
+  --template-body file://cfn/eks-vpc.yml\
+  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
+  --region=us-west-2

c461abb931f0dc288649785e91d54d04927627c1
Thomas Weibel <thom@ikhono.net>
2020-05-11 12:44:00 +0200

    Update scripts to use specific parameters file

--- cfn/01-create-vpc-stack.sh
+++ cfn/01-create-vpc-stack.sh
@@ -2,6 +2,6 @@
 aws cloudformation create-stack\
   --stack-name eks-vpc-stack\
   --template-body file://cfn/eks-vpc.yml\
-  --parameters file://cfn/parameters.json \
+  --parameters file://cfn/eks-vpc-parameters.json \
   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
   --region=us-west-2
--- cfn/02-create-eks-cluster-stack.sh
+++ cfn/02-create-eks-cluster-stack.sh
@@ -2,6 +2,6 @@
 aws cloudformation create-stack\
   --stack-name eks-cluster-stack\
   --template-body file://cfn/eks-cluster.yml\
-  --parameters file://cfn/parameters.json \
+  --parameters file://cfn/eks-cluster-parameters.json \
   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"\
   --region=us-west-2

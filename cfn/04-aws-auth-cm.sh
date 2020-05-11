8c15e1fe12ed19ed621cd26156bf9827af074263
Thomas Weibel <thom@ikhono.net>
2020-05-11 17:17:46 +0200

    Add comment

--- cfn/04-aws-auth-cm.sh
+++ cfn/04-aws-auth-cm.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
-
+# 4. Enable worker nodes to join your cluster
 aws eks --region us-west-2 update-kubeconfig --name EKSCluster-K2C0YJs3qYCP
 kubectl apply -f aws-auth-cm.yml
 kubectl get nodes --watch
